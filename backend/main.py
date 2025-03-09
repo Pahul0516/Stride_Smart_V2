503e05562e889519452fc761187498ce790c2ce4
Lari631 <munteanlarisamaria@gmail.com>
2025-03-09 17:40:06 +0200

    removed comments

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,4 @@ from app.factory import create_app
 
 if __name__ == '__main__':
     Aplication = create_app()
-    #app = Flask(__name__)
-    #CORS(Application)
     Aplication.run(debug=False, port=5501)
