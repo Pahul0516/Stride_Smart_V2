aebfc948412e5fd9a511ee78c853fcf94ff1be5f
Lari631 <munteanlarisamaria@gmail.com>
2025-03-09 17:40:06 +0200

    removed comments

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,12 @@ from app.factory import create_app
 if __name__ == '__main__':
     Aplication = create_app()
 <<<<<<< HEAD
+<<<<<<< HEAD
     Aplication.run(debug=False, port=5001)
 =======
     #app = Flask(__name__)
     #CORS(Application)
+=======
+>>>>>>> 503e055 (removed comments)
     Aplication.run(debug=False, port=5501)
 >>>>>>> 243604a (Nature Path)
